38e0083ed83069f0d0acace1af974ea501578041
Saldana <lmsaldana@cps.k12.il.us>
2024-10-01 08:03:40 -0500

    first commit

---
+++ week5/sets_tupous_list.py
@@ -0,0 +1,82 @@
+#collection=single "variable" used to store multiple values 
+# List = [] ordered and changeable. Duplicates Ok 
+#Set= {}unordered and imutable , but add/remove OK . No du-plicates 
+#Tuple=() ordered amd unchangeble . Duplicates OK. Faster 
+
+# Fruit= [ " apples", "oranges" ,"bannana","coconut"]
+# print (dir (fruits))
+
+# print(help(fruits))
+# print (len (fruits))
+# print (fruits[0] )
+# print ("pineapple" in fruits)
+
+# for x in fruits :
+# print (x)
+
+# fruits[1]="pineapple"
+#print (fruits [0])
+#for fruits.append ("pineapple")
+#for fuits.remove ("apple")
+#for fruit .insert (0,"pineapple")
+#fruits.reverse()
+# #fruits.clear ()
+# print(fruits)
+# # for fruits in fruits;
+
+# cars=["bmw","masserati"," Audi", "mercedes","ferrari"]
+# print ( f" these are list of {cars}")
+# print (f"the first car is {cars[0']}")
+                                
+# #changing the value of the list 
+# cars[0]="toyota"
+# print ( f"the first car is {cars[0]}")
+
+# print (f "the last car is {cars [-1] }")
+# cars [-1] = "lamborginah"
+# print(f"the first car is {cars[-1]}")
+
+# #adding a new value to the list 
+# cars.append ("bugatti")
+# print(cars)
+# cars.remove ("maseratti")
+# print(cars)
+
+# #looping through the list 
+# #otherwise called litering through the list 
+# for car in cars :
+# #print (len(car))
+# #print(car)
+# #cars=input ("add a new car please :")
+# #cars.append (carRequest)
+# print(len (cars))
+# cars(cars.uppercase())
+# print(cars)
+
+# #challenge 
+# #create a list of friends
+
+# make sure the initial list is none 
+# friends =[]
+# #add a new friend to the list , add at least 5 friends 
+# #remove a friend 
+# #insert a friend at a specefic index mabye 2
+# #print the list of friends 
+# #loop through the list and print the friends name 
+# #see if the particular friend is in the list (boolean value)
+# #if the lists greater then 10 break the loop 
+
+friends = ["natalie","Lucy","lily","natalia" ,"erik"]
+print(friends)
+friends.remove ("natalie")
+friends.apprend ("areil",[3])
+print (friends )
+
+if "Lucy" in friends :
+    print ("Lucy is in list")
+
+for friends in friends :
+    print(friends)
+    print(len(friends)) >10:
+    break
+  print(friends)
